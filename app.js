2c8eb7eb39bdcf769b466d68297ffe8fab310207
Ruben Mateas <rubenmaetas1313@outlook.com>
2018-02-06 22:36:13 -0800

    js displays the required content on the page

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
-var planetNames = ["Jupiter", "Venus", "Saturn", "Mars", "Uncreative"];
-var distancesFromEarth = [5.2, 0.72, 9.5, 1.5, 42.0];
-var sunnyInAugust = [false, true, false, true, false];
+window.onload = function() {
 
-//Display the planet information
-for(var i = 0; i < planetNames.length; i++)
-{
-  console.log(planetNames[i]);
-  console.log(distancesFromEarth[i]);
-  console.log(sunnyInAugust[i]);
-  console.log("----------");
-}
+  var planetNames = ["Jupiter", "Venus", "Saturn", "Mars", "Uncreative"];
+  var distancesFromEarth = [5.2, 0.72, 9.5, 1.5, 42.0];
+  var sunnyInAugust = [false, true, false, true, false];
 
-//Find the sunny planets
-
-console.log("These planets are sunny in August:");
-
-for(var i = 0; i < planetNames.length; i++)
-{
-  if(sunnyInAugust[i])
+  //Display the planet information
+  for(var i = 0; i < planetNames.length; i++)
   {
     console.log(planetNames[i]);
+    console.log(distancesFromEarth[i]);
+    console.log(sunnyInAugust[i]);
+    console.log("----------");
   }
-}
 
-//Find the closest planet
+  //Find the sunny planets
 
-var closestPlanet = "";
-var shortestDistance = 10;
+  console.log("These planets are sunny in August:");
 
-for(var i = 0; i < planetNames.length; i++)
-{
-  if(distancesFromEarth[i] < shortestDistance)
+  for(var i = 0; i < planetNames.length; i++)
   {
-    shortestDistance = distancesFromEarth[i];
-    closestPlanet = planetNames[i];
+    if(sunnyInAugust[i])
+    {
+      console.log(planetNames[i]);
+    }
   }
-}
 
-console.log("Closest planet: " + closestPlanet);
-console.log("Shortest distance: " + shortestDistance);
+  //Find the closest planet
+
+  var closestPlanet = "";
+  var shortestDistance = 10;
+
+  for(var i = 0; i < planetNames.length; i++)
+  {
+    if(distancesFromEarth[i] < shortestDistance)
+    {
+      shortestDistance = distancesFromEarth[i];
+      closestPlanet = planetNames[i];
+    }
+  }
+
+  console.log("Closest planet: " + closestPlanet);
+  console.log("Shortest distance: " + shortestDistance);
+
+  var insert= "";
+
+  for (var i = 0; i < planetNames.length; i++) {
+    insert += "<span class='planet'>" + planetNames[i] + "</span>" + "<br />" + distancesFromEarth[i] + "<br />" + sunnyInAugust[i] + "<br />" + "-------- <br />";
+    console.log(insert);
+  }
+
+  document.getElementById("main").innerHTML = "" + insert + "";
+}
